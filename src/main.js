1d8c93c352fac6df94754c4812eaa250983aa932
Ramiro <ramiro.depaoli@gmail.com>
2024-11-03 16:35:45 -0300

    mejoras

--- src/main.js
+++ src/main.js
@@ -18,9 +18,7 @@ const port = process.env.APP_PORT || 3000;
 const accessUsers = process.env.ACCESS_USERS ? process.env.ACCESS_USERS.split(', ') : [];
 const masterKeys = process.env.MASTER_KEYS ? process.env.MASTER_KEYS.split(', ') : [];
 
-if (!process.env.SESSION_SECRET) {
-    throw new Error('SESSION_SECRET no está definido en el archivo .env');
-}
+if (!process.env.SESSION_SECRET) throw new Error('SESSION_SECRET no está definido en el archivo .env');
 
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET,
@@ -32,16 +30,14 @@ app.use(express.static('src/public'));
 app.use(sessionMiddleware);
 io.engine.use(sessionMiddleware);
 
-let waiting = false;
+let currentData = null;
 io.on('connection', (socket) => {
     console.log('Usuario conectado');
     const session = socket.request.session;
 
     if (session.username) {
         socket.emit('loggedIn', session.username);
-        if (waiting) {
-            socket.emit('waitConfirmation', true);
-        }
+        if (currentData) socket.emit('waitConfirmation', true);
     }
 
     socket.on('disconnect', () => {
@@ -54,24 +50,14 @@ io.on('connection', (socket) => {
 
     socket.on('login', (data) => {
         try {
-            if (session.username) {
-                throw 'Ya está logeado';
-            }
+            if (session.username) throw 'Ya está logeado';
             const { username, password } = data;
-            if (!(username && password)) {
-                throw 'Usuario y contraseña son requeridos'
-            }
-            if (!(accessUsers.includes(username) && masterKeys.includes(password))) {
-                throw 'Usuario o contraseña incorrectos'
-            }
+            if (!(username && password)) throw 'Usuario y contraseña son requeridos';
+            if (!(accessUsers.includes(username) && masterKeys.includes(password))) throw 'Usuario o contraseña incorrectos';
             session.username = username;
             session.save();
             socket.emit('loggedIn', username);
-            if (waiting) {
-                socket.emit('waitConfirmation', true);
-            } else {
-                socket.emit('sendAlert', { success: true, message: '¡Inicio de sesión exitoso!' });
-            }
+            if (currentData) socket.emit('waitConfirmation', true);
         } catch (error) {
             socket.emit('sendAlert', { success: false, message: error.message || error || 'Error desconocido' });
         }
@@ -79,9 +65,7 @@ io.on('connection', (socket) => {
 
     socket.on('logout', () => {
         try {
-            if (!session.username) {
-                throw 'No está logeado';
-            }
+            if (!session.username) throw 'No está logeado';
             session.destroy();
             socket.emit('loggedOut', true);
             socket.emit('sendAlert', { success: true, message: '¡Cierre de sesión exitoso!' });
@@ -92,20 +76,16 @@ io.on('connection', (socket) => {
 
     socket.on('add', async (data) => {
         try {
-            if (!session.username) {
-                throw 'No está logeado';
-            }
+            if (!session.username) throw 'No está logeado';
             const nombre = data.nombre;
-            if (!nombre) {
-                throw 'Nombre es requerido';
-            }
+            if (!nombre) throw 'Nombre es requerido';
             const huella = await DatosCliente.create({ nombre });
             io.emit('enviarHuella', { nombre: nombre, operacion: 0 });
-            waiting = true;
+            currentData = huella;
             io.emit('waitConfirmation', true);
             setTimeout(() => {
-                if (waiting) {
-                    waiting = false;
+                if (currentData) {
+                    currentData = null;
                     io.emit('waitConfirmation', false);
                     huella.destroy();
                     io.emit('sendAlert', { success: false, message: '¡No hay respuesta!' });
@@ -118,23 +98,17 @@ io.on('connection', (socket) => {
 
     socket.on('delete', async (data) => {
         try {
-            if (!session.username) {
-                throw 'No está logeado';
-            }
+            if (!session.username) throw 'No está logeado';
             const nombre = data.nombre;
-            if (!nombre) {
-                throw 'Nombre es requerido';
-            }
+            if (!nombre) throw 'Nombre es requerido';
             const huella = await DatosCliente.findOne({ where: { nombre: nombre } });
-            if (!huella) {
-                throw 'No se encontró con ese nombre';
-            }
+            if (!huella) throw 'No se encontró con ese nombre';
             io.emit('enviarHuella', { nombre: nombre, operacion: 1, huella: huella.huella });
-            waiting = true;
+            currentData = huella;
             io.emit('waitConfirmation', true);
             setTimeout(() => {
-                if (waiting) {
-                    waiting = false;
+                if (currentData) {
+                    currentData = null;
                     io.emit('waitConfirmation', false);
                     io.emit('sendAlert', { success: false, message: '¡No hay respuesta!' });
                 }
@@ -146,23 +120,17 @@ io.on('connection', (socket) => {
 
     socket.on('edit', async (data) => {
         try {
-            if (!session.username) {
-                throw 'No está logeado';
-            }
+            if (!session.username) throw 'No está logeado';
             const { nombre, numero } = data;
-            if (!(nombre && numero)) {
-                throw 'Nombre y Número son requeridos';
-            }
+            if (!(nombre && numero)) throw 'Nombre y Número son requeridos';
             const huella = await DatosCliente.findOne({ where: { huella: numero } });
-            if (!huella) {
-                throw 'No se encontró con ese número';
-            }
+            if (!huella) throw 'No se encontró con ese número';
             io.emit('enviarHuella', { nombre: nombre, operacion: 2, huella: huella.huella });
-            waiting = true;
+            currentData = huella;
             io.emit('waitConfirmation', true);
             setTimeout(() => {
-                if (waiting) {
-                    waiting = false;
+                if (currentData) {
+                    currentData = null;
                     io.emit('waitConfirmation', false);
                     io.emit('sendAlert', { success: false, message: '¡No hay respuesta!' });
                 }
@@ -174,15 +142,13 @@ io.on('connection', (socket) => {
 
     socket.on('vaciar', async () => {
         try {
-            if (!session.username) {
-                throw 'No está logeado';
-            }
+            if (!session.username) throw 'No está logeado';
             io.emit('vacioDatabase', true);
-            waiting = true;
+            currentData = true;
             io.emit('waitConfirmation', true);
             setTimeout(() => {
-                if (waiting) {
-                    waiting = false;
+                if (currentData) {
+                    currentData = null;
                     io.emit('waitConfirmation', false);
                     io.emit('sendAlert', { success: false, message: '¡No hay respuesta!' });
                 }
@@ -194,36 +160,39 @@ io.on('connection', (socket) => {
 
     socket.on('confirmacionHuella', async (data) => {
         try {
-            waiting = false;
-            io.emit('waitConfirmation', false);
             if (data.error != 'sin errores') {
+                if (data.operacion == 0) {
+                    await currentData.destroy();
+                }
                 throw data.error;
             }
-            const huella = await DatosCliente.findOne({ where: { huella: data.huella } });
-            if (!huella) {
-                throw 'No se encontró con ese número';
-            }
+            let currentMessage;
             if (data.operacion == 0) {
-                await huella.update({ huella: data.huella });
-                io.emit('sendAlert', { success: true, message: '¡Se guardó con éxito!' });
+                await currentData.update({ huella: data.huella });
+                currentMessage = '¡Se guardó con éxito!';
             } else if (data.operacion == 1) {
-                await huella.destroy();
-                io.emit('sendAlert', { success: true, message: '¡Se eliminó con éxito!' });
+                await currentData.destroy();
+                currentMessage = '¡Se eliminó con éxito!';
             } else if (data.operacion == 2) {
-                await huella.update({ nombre: data.nombre });
-                io.emit('sendAlert', { success: true, message: '¡Se editó con éxito!' });
+                await currentData.update({ nombre: data.nombre });
+                currentMessage = '¡Se editó con éxito!';
             } else {
                 throw 'No se encontró esa operación';
             }
+            currentData = null;
+            io.emit('waitConfirmation', false);
+            io.emit('sendAlert', { success: true, message: currentMessage });
         } catch (error) {
-            socket.emit('sendAlert', { success: false, message: error.message || error || 'Error desconocido' });
+            currentData = null;
+            io.emit('waitConfirmation', false);
+            io.emit('sendAlert', { success: false, message: error.message || error || 'Error desconocido' });
         }
     });
 
     socket.on('confirmacionVaciar', async () => {
-        waiting = false;
-        io.emit('waitConfirmation', false);
         await DatosCliente.destroy({ truncate: true });
+        currentData = null;
+        io.emit('waitConfirmation', false);
         io.emit('sendAlert', { success: true, message: '¡Se vacio con éxito!' });
     });
 
